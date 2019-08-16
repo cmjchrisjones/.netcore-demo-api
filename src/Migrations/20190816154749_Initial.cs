d29b49e107c644f2b52ea310d90eca6650fab095
Chris Jones <cmj.chris.jones@googlemail.com>
2019-08-16 16:59:53 +0100

    Update 20190816154749_Initial.cs

--- src/Migrations/20190816154749_Initial.cs
+++ src/Migrations/20190816154749_Initial.cs
@@ -3,21 +3,56 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DemoAPI.Migrations
 {
-    public partial class Initial2 : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "RequestDate",
-                table: "Requests",
-                nullable: true);
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RequesterName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    RequestDate = table.Column<DateTime>(nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false),
+                    RequestId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Item_Requests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Requests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_RequestId",
+                table: "Item",
+                column: "RequestId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "RequestDate",
-                table: "Requests");
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
         }
     }
 }
